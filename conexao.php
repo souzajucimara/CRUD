6fb492d6cb00ae532fbb10c362df19dec1fdb307
Jucimara Souza <98478692+souzajucimara@users.noreply.github.com>
2022-04-09 17:33:42 -0300

    Create conexao.php
    Essa página cria uma conexão com o banco de dados "universidade".

---
+++ conexao.php
@@ -0,0 +1,9 @@
+<?php 
+$host = 'localhost'; 
+$user = 'root';
+$password = '';
+$bd_name = 'universidade.php';
+
+// Conexão com o Banco de dados
+$conex = mysqli_connect($host, $user, $password, $bd_name);
+?>
